0cd6bfa6fdedd32e66b24ef93911a4fb89259d10
Manasa <manasagopal.93@gmail.com>
2014-03-29 12:14:54 +0530

    To add back the Ner and Pos tagger and update feature

---
+++ src/com/appointext/nertagger/NERecognizer.java
@@ -0,0 +1,24 @@
+package com.appointext.nertagger;
+import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+import edu.stanford.nlp.ie.crf.*;
+import edu.stanford.nlp.ling.CoreLabel;
+import java.io.IOException;
+
+
+public class NERecognizer {
+
+    public static String NERTagger(String message) throws IOException {
+
+      String serializedClassifier = "classifiers/english.all.3class.distsim.crf.ser.gz";
+
+      AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifierNoExceptions(serializedClassifier);
+
+      //The output of the NER can be displayed in 3 ways and that is called in the following lines
+      
+      return(classifier.classifyToString(message));
+      //return(classifier.classifyWithInlineXML(message));
+      //return(classifier.classifyToString(message, "xml", true));
+        
+      }
+ 
+}
